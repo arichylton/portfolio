2e514fe5fffa0760e48260524bdb8e2d3e441adc
Aric Hylton <arich212@gmail.com>
2023-05-29 22:18:02 -0500

    made some small file changes

--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -5,3 +5,4 @@ export { default as Skills } from './skills/Skills';
 export { default as About } from './about/About';
 export { default as Contact } from './contact/Contact';
 export { default as Footer } from './footer/Footer';
+
